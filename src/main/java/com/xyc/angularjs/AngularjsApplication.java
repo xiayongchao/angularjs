5e489bd50e9b949ecb36ccad7daa6a1a4038e6cd
xiayongchao <2388604953@qq.com>
2017-06-11 10:14:15 +0800

    初始代码

---
+++ src/main/java/com/xyc/angularjs/AngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.xyc.angularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularjsApplication.class, args);
+	}
+}
